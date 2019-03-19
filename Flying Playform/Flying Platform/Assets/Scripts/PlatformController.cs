a291386713c1a69d991ef811904ab94afe2a14b5
Gerardo Vela <gvelaxv@gmail.com>
2019-03-18 23:04:04 -0600

    Cardboard uploaded

--- Flying Playform/Flying Platform/Assets/Scripts/PlatformController.cs
+++ Flying Playform/Flying Platform/Assets/Scripts/PlatformController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlatformController : MonoBehaviour
 {
-    // movement target
-    public Transform target;
+    // destinations /targets
+    public Transform[] targets;
 
     // speed
     public float speed = 1f;
@@ -13,10 +13,17 @@ public class PlatformController : MonoBehaviour
     // flag that sets whether we are moving or not
     bool isMoving = false;
 
+    // next destination indes
+    int nextIndex;
+
 	// Use this for initialization
 	void Start ()
     {
-		
+        // set player to first target
+        transform.position = targets[0].position;
+
+        // next destination is 1
+        nextIndex = 1;
 	}
 	
 	// Update is called once per frame
@@ -35,14 +42,29 @@ public class PlatformController : MonoBehaviour
         if (isMoving)
         {
             // calculate teh distance from targetr
-            float distance = Vector3.Distance(transform.position, target.position);
+            float distance = Vector3.Distance(transform.position, targets[nextIndex].position);
             // have we arrived?
             if (distance > 0)
             {
                 // calcualte how much we need to move(step) d = v * t
                 float step = speed * Time.deltaTime;
                 // move by that step
-                transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+                transform.position = Vector3.MoveTowards(transform.position, targets[nextIndex].position, step);
+            }
+            // if we have arrived we should update nextIndex
+            else
+            {
+                // next index is increased by 1
+                nextIndex++;
+
+                // array element index starts at 0 and goes all the way to length -1
+                if (nextIndex == targets.Length)
+                {
+                    nextIndex = 0;
+                }
+
+                // stop moving
+                isMoving = false;
             }
         }
     }
