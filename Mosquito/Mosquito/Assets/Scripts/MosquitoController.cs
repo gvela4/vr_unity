4adc119519c231d625691e8f293cdf6ebc68669b
Gerardo Vela <gvelaxv@gmail.com>
2019-04-11 19:06:14 -0600

    section 3

---
+++ Mosquito/Mosquito/Assets/Scripts/MosquitoController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRStandardAssets.Utils;
+
+public class MosquitoController : MonoBehaviour
+{
+    // moving speed
+    public float speed = 0.5f;
+
+    // minimum distance from us
+    public float minDistance = 0.55f;
+
+    // vr interactive item
+    VRInteractiveItem vrIntItem;
+
+    // rigibody
+    Rigidbody rb;
+
+    // flag to keep track whether the mosquito is moving
+    bool isMoving = true;
+
+    // target 
+    Vector3 target;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        // grab vr interactive component
+        vrIntItem = GetComponent<VRInteractiveItem>();
+
+        // grab rigibody component
+        rb = GetComponent<Rigidbody>();
+
+        // set target
+        target = Camera.main.transform.position;
+
+        // make mosquito look at us
+        transform.LookAt(target);
+	}
+
+    // when our game object is enabled
+    void OnEnable()
+    {
+        vrIntItem.OnClick += HandleClick;
+    }
+
+    // when the game object is disabled 
+    void OnDisable()
+    {
+        vrIntItem.OnClick -= HandleClick;
+    }
+
+    // this is called when the mosquito is clicked on
+    void HandleClick()
+    {
+        // check if it's moving
+        if (rb.isKinematic)
+        {
+            // rotate transform
+            transform.Rotate(new Vector3(0, 0, 180));
+
+            // disable kinematic property
+            rb.isKinematic = false;
+
+            // set flag to false
+            isMoving = false;
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update ()
+    {
+        // check that we are moving
+        if (isMoving)
+        {
+            // calculate distance from target
+            float distance = Vector3.Distance(transform.position, target);
+
+            // check min distance
+            if (distance <= minDistance)
+            {
+                // we stop
+                isMoving = false;
+            }
+            else
+            {
+                // calcualte movement step v = d/t
+                float movementStep = speed * Time.deltaTime;
+
+                // move on that step
+                transform.position = Vector3.MoveTowards(transform.position, target, movementStep);
+            }
+        }
+	}
+}
---
+++ Mosquito/Mosquito/Assets/Scripts/TreeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeController : MonoBehaviour
+{
+    // min and max scale values
+    public float minScale = 0.7f;
+    public float maxScale = 2.4f;
+	// Use this for initialization
+	void Start ()
+    {
+        // set Y position to ground level
+        transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+
+        // obtain a random scale
+        float scale = Random.Range(minScale, maxScale);
+
+        // change scale or *= scale
+        transform.localScale = scale * transform.localScale;
+
+        // random rotation value
+        float rotationY = Random.Range(0, 360);
+
+        // rotate about up y axis
+        transform.Rotate(0, rotationY, 0, Space.World);
+
+	}
+}
